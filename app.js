de4667bd284762bffc181e59e37c404de444e380
TommyCakes <tomhwarhurst@aol.com>
2015-08-12 16:59:22 +0100

    Added gender choice

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
-angular.module('myApp', [ ])
+var app = angular.module('myApp', [ ]);
+  // app.controller('myCtrl', function($scope) {
+  //   $scope.maleName = "Tommy"
+
+  app.controller('myCtrl', function() {
+    var self = this;
+    this.maleName = "{ Male Name }";
+    this.femaleName = "{ Female Name }";
+    this.jobTitle = " { Job Title}";
+    this.tediousTask = " { Tedious Task}";
+    this.dirtyTask =  " { Dirty Task}";
+    this.celebrity =  " { Celebrity}";
+    this.uselessSkill =  " { Useless Skill}";
+    this.adjective =  " { Adjective}";
+    this.obnoxiousCelebrity =  " { Obnoxius Celebrity}";
+    this.hugeNumber = " { Huge Number}";
+
+    this.gender = "";
+
+    this.genderChoice = function(choice){
+      if ( choice === "male") {
+        self.gender = "male"
+      }
+      else {
+      self.gender = "female"
+      }
+
+    };
+  });
